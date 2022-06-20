3c2243ac1f7fbffaa4d065d4b9fdf501dcda0058
shvec-es <kotova.es.1510@gmail.com>
2022-06-20 16:46:21 +0300

    Add and get own words

--- src/users/user.schema.ts
+++ src/users/user.schema.ts
@@ -25,8 +25,6 @@ export class User {
 
   @Prop({ default: ['USER']})
   roles: [];
-
-
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
--- src/words/words.controller.ts
+++ src/words/words.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Post, Body, Get, UseGuards } from '@nestjs/common';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Controller, Post, Body, Get, UseGuards, Req } from '@nestjs/common';
+import { ApiCreatedResponse, ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { WordsService } from './words.service';
 import { CreateWordDto } from './dto/create-word.dto';
 import { GetAllWordsDto } from './dto/get-all-words.dto';
@@ -12,19 +12,26 @@ export class WordsController {
     constructor(private wordsService: WordsService) { }
     
     @ApiOperation({summary: "Add new word"})
-    @ApiResponse({ status: 201, type: CreateWordDto })
+    @ApiCreatedResponse({ type: CreateWordDto })
     @Post('/create')
     @UseGuards(JwtAuthGuard)
-
-    create(@Body() createWordDto: CreateWordDto) {
-        return this.wordsService.create(createWordDto);
+    create(@Body() createWordDto: CreateWordDto, @Req() req: any) {
+        return this.wordsService.create(createWordDto, req.user.id);
     }
 
     @ApiOperation({summary: "Get all words"})
-    @ApiResponse({type: GetAllWordsDto})
-    @Get('/getallwords')
+    @ApiOkResponse({type: GetAllWordsDto})
+    @Get('/getall')
     @UseGuards(JwtAuthGuard)
     getAll() {
         return this.wordsService.getAll();
+    }
+
+    @ApiOperation({summary: "Own vocabulary"})
+    @ApiOkResponse({type: GetAllWordsDto})
+    @Get('/vocabulary')
+    @UseGuards(JwtAuthGuard)
+    getOwnWords(@Req() req: any) {
+        return this.wordsService.getOwnWords(req.user.id);
     }
 }
