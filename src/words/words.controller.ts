bb1061a9ed64d72dd6f6294d574b7248d247469f
Dima Sotskiy <sotskiydr@gmail.com>
2022-07-07 14:38:36 +0300

    resolve conflicts

--- src/words/words.controller.ts
+++ src/words/words.controller.ts
@@ -1,6 +1,6 @@
 
-import { Controller, Post, Body, Get, UseGuards, Req } from "@nestjs/common";
-import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
+import { Controller, Post, Body, Get, UseGuards, Req, Put, Param, Delete } from "@nestjs/common";
+import { ApiCreatedResponse, ApiOkResponse, ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { WordsService } from "./words.service";
 import { CreateWordDto } from "./dto/create-word.dto";
 import { GetAllWordsDto } from "./dto/get-all-words.dto";
@@ -10,14 +10,15 @@ import { JwtAuthGuard } from "../auth/jwt-auth.guard";
 @ApiTags("Words")
 @Controller("words")
 export class WordsController {
-  constructor(private wordsService: WordsService) {}
+  constructor(private wordsService: WordsService) {
+  }
 
-  @ApiOperation({ summary: "Add new word" })
-  @ApiResponse({ status: 201, type: CreateWordDto })
-  @Post("/create")
+  @ApiOperation({summary: "Add new word"})
+  @ApiCreatedResponse({ type: CreateWordDto })
+  @Post('/create')
   @UseGuards(JwtAuthGuard)
-  create(@Body() createWordDto: CreateWordDto) {
-    return this.wordsService.create(createWordDto);
+  create(@Body() createWordDto: CreateWordDto, @Req() req: any) {
+    return this.wordsService.create(createWordDto, req.user.id);
   }
 
   @ApiOperation({ summary: "Get all words" })
@@ -27,30 +28,30 @@ export class WordsController {
   getAll() {
     return this.wordsService.getAll();
   }
-  
- @ApiOperation({summary: "Own vocabulary"})
-    @ApiOkResponse({type: GetAllWordsDto})
-    @Get('/vocabulary')
-    @UseGuards(JwtAuthGuard)
-    getOwnWords(@Req() req: any) {
-        return this.wordsService.getOwnWords(req.user.id);
-    }
 
-    @ApiOperation({summary: "Update word"})
-    @ApiOkResponse({type: UpdateWordDto})
-    @Put('/:id')
-    @UseGuards(JwtAuthGuard)
-    updateWord(@Param('id') id: number, @Body() updateWordDto: UpdateWordDto) {
-        return this.wordsService.updateWord(id, updateWordDto);
-    }
+  @ApiOperation({ summary: "Own vocabulary" })
+  @ApiOkResponse({ type: GetAllWordsDto })
+  @Get('/vocabulary')
+  @UseGuards(JwtAuthGuard)
+  getOwnWords(@Req() req: any) {
+    return this.wordsService.getOwnWords(req.user.id);
+  }
 
-    @ApiOperation({summary: "Delete word"})
-    @ApiOkResponse({description: 'Deleted successfuly'})
-    @Delete('/:id')
-    @UseGuards(JwtAuthGuard)
-    deleteWord(@Param('id') id: number) {
-        return this.wordsService.deleteWord(id);
-    }
+  @ApiOperation({ summary: "Update word" })
+  @ApiOkResponse({ type: UpdateWordDto })
+  @Put('/:id')
+  @UseGuards(JwtAuthGuard)
+  updateWord(@Param('id') id: number, @Body() updateWordDto: UpdateWordDto) {
+    return this.wordsService.updateWord(id, updateWordDto);
+  }
+
+  @ApiOperation({ summary: "Delete word" })
+  @ApiOkResponse({ description: 'Deleted successfuly' })
+  @Delete('/:id')
+  @UseGuards(JwtAuthGuard)
+  deleteWord(@Param('id') id: number) {
+    return this.wordsService.deleteWord(id);
+  }
 
   @Get("/get-admins-words")
   @ApiResponse({ type: GetAllWordsDto })
@@ -58,3 +59,5 @@ export class WordsController {
   getAdminsWords(@Req() user) {
     return this.wordsService.getAdminsWords(user.user.email);
   }
+
+}
--- src/words/words.service.ts
+++ src/words/words.service.ts
@@ -1,12 +1,12 @@
-import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable, NotFoundException } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { CreateWordDto } from "./dto/create-word.dto";
 import { Word, WordDocument } from "./word.schema";
 import { UpdateWordDto } from './dto/update-word.dto';
-import { Word, WordDocument } from './word.schema';
 import adminsWords from "../../data/voc.json";
 import { UsersService } from "../users/users.service";
+import { User } from "../users/user.schema";
 
 
 @Injectable()
@@ -15,18 +15,6 @@ export class WordsService {
     @InjectModel(Word.name) private wordSchema: Model<WordDocument>,
     private userService: UsersService,
   ) {}
-
-
-    async create(wordDto: CreateWordDto, owner: User) {
-            const newWord =  new this.wordSchema({...wordDto, owner})
-            const existWord = await this.wordSchema.findOne({wordEn: newWord.wordEn}).exec();
-            if (existWord) {
-                throw new HttpException(`Word ${newWord.wordEn} already exist`, HttpStatus.CONFLICT) 
-            }
-            await newWord.save()
-            return newWord    
-    }
-  }
 
   async getAll() {
     const allWords = await this.wordSchema.find();
@@ -40,28 +28,37 @@ export class WordsService {
   }
 
 
-    async getOwnWords(id: number) {
-        const myVocabulary = await this.wordSchema.find({ owner: id })
-        if (myVocabulary) {
-             return myVocabulary;
-        }
-       throw new HttpException('Add words to your own vocabulary', HttpStatus.NOT_FOUND)
+  async getOwnWords(id: number) {
+    const myVocabulary = await this.wordSchema.find({ owner: id })
+    if (myVocabulary) {
+      return myVocabulary;
     }
+    throw new HttpException('Add words to your own vocabulary', HttpStatus.NOT_FOUND)
+  }
 
-    async updateWord(id: number, updateWord: UpdateWordDto) {
-        const updatedWord = await this.wordSchema.findByIdAndUpdate(id, updateWord);
-        if (!updatedWord) {
-            throw new NotFoundException();
-        }
-        return updatedWord;
+  async updateWord(id: number, updateWord: UpdateWordDto) {
+    const updatedWord = await this.wordSchema.findByIdAndUpdate(id, updateWord);
+    if (!updatedWord) {
+      throw new NotFoundException();
     }
+    return updatedWord;
+  }
 
-    async deleteWord(id: number) {
-        const deletedWord = await this.wordSchema.findByIdAndDelete(id);
-        if (!deletedWord) {
-            throw new NotFoundException();
-        }
-        return "Deleted successfuly"
+  async deleteWord(id: number) {
+    const deletedWord = await this.wordSchema.findByIdAndDelete(id);
+    if (!deletedWord) {
+      throw new NotFoundException();
     }
+    return "Deleted successfuly"
+  }
 
+  async create(wordDto: CreateWordDto, owner: User) {
+      const newWord =  new this.wordSchema({...wordDto, owner})
+      const existWord = await this.wordSchema.findOne({wordEn: newWord.wordEn}).exec();
+      if (existWord) {
+          throw new HttpException(`Word ${newWord.wordEn} already exist`, HttpStatus.CONFLICT)
+      }
+      await newWord.save()
+      return newWord
+  }
 }
