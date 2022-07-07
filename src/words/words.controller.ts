a09a0803907e9816eb53cdac4517e6bf3b466911
Dima Sotskiy <sotskiydr@gmail.com>
2022-07-07 14:25:16 +0300

    add admins role , add admins data

--- src/words/words.controller.ts
+++ src/words/words.controller.ts
@@ -1,30 +1,35 @@
-import { Controller, Post, Body, Get, UseGuards } from '@nestjs/common';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { WordsService } from './words.service';
-import { CreateWordDto } from './dto/create-word.dto';
-import { GetAllWordsDto } from './dto/get-all-words.dto';
+import { Controller, Post, Body, Get, UseGuards, Req } from "@nestjs/common";
+import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
+import { WordsService } from "./words.service";
+import { CreateWordDto } from "./dto/create-word.dto";
+import { GetAllWordsDto } from "./dto/get-all-words.dto";
 import { JwtAuthGuard } from "../auth/jwt-auth.guard";
 
-@ApiTags('Words')
-@Controller('words')
+@ApiTags("Words")
+@Controller("words")
 export class WordsController {
+  constructor(private wordsService: WordsService) {}
 
-    constructor(private wordsService: WordsService) { }
-    
-    @ApiOperation({summary: "Add new word"})
-    @ApiResponse({ status: 201, type: CreateWordDto })
-    @Post('/create')
-    @UseGuards(JwtAuthGuard)
+  @ApiOperation({ summary: "Add new word" })
+  @ApiResponse({ status: 201, type: CreateWordDto })
+  @Post("/create")
+  @UseGuards(JwtAuthGuard)
+  create(@Body() createWordDto: CreateWordDto) {
+    return this.wordsService.create(createWordDto);
+  }
 
-    create(@Body() createWordDto: CreateWordDto) {
-        return this.wordsService.create(createWordDto);
-    }
+  @ApiOperation({ summary: "Get all words" })
+  @ApiResponse({ type: GetAllWordsDto })
+  @Get("/getallwords")
+  @UseGuards(JwtAuthGuard)
+  getAll() {
+    return this.wordsService.getAll();
+  }
 
-    @ApiOperation({summary: "Get all words"})
-    @ApiResponse({type: GetAllWordsDto})
-    @Get('/getallwords')
-    @UseGuards(JwtAuthGuard)
-    getAll() {
-        return this.wordsService.getAll();
-    }
+  @Get("/get-admins-words")
+  @ApiResponse({ type: GetAllWordsDto })
+  @UseGuards(JwtAuthGuard)
+  getAdminsWords(@Req() user) {
+    return this.wordsService.getAdminsWords(user.user.email);
+  }
 }
--- src/words/words.service.ts
+++ src/words/words.service.ts
@@ -1,30 +1,45 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
-import { CreateWordDto } from './dto/create-word.dto';
-import { Word, WordDocument } from './word.schema';
+import { CreateWordDto } from "./dto/create-word.dto";
+import { Word, WordDocument } from "./word.schema";
+import adminsWords from "../../data/voc.json";
+import { UsersService } from "../users/users.service";
 
 @Injectable()
 export class WordsService {
-  constructor(@InjectModel(Word.name) private wordSchema: Model<WordDocument>) {}
+  constructor(
+    @InjectModel(Word.name) private wordSchema: Model<WordDocument>,
+    private userService: UsersService,
+  ) {}
 
-    async create(wordDto: CreateWordDto) {
-        try {
-            const newWord = new this.wordSchema(wordDto)
-            const existWord = await this.wordSchema.findOne({wordEn: newWord.wordEn}).exec();
-            if (existWord) {
-                throw new HttpException(`${newWord.wordEn} already exist`, HttpStatus.CONFLICT) 
-            }
-            await newWord.save()
-            return newWord 
-        } catch (error) {
-            console.log(error.message);
-        }
-        
+  async create(wordDto: CreateWordDto) {
+    try {
+      const newWord = new this.wordSchema(wordDto);
+      const existWord = await this.wordSchema
+        .findOne({ wordEn: newWord.wordEn })
+        .exec();
+      if (existWord) {
+        throw new HttpException(
+          `${newWord.wordEn} already exist`,
+          HttpStatus.CONFLICT
+        );
+      }
+      await newWord.save();
+      return newWord;
+    } catch (error) {
+      console.log(error.message);
     }
+  }
 
-    async getAll() {
-        const allWords = await this.wordSchema.find()
-    return allWords
-}
+  async getAll() {
+    const allWords = await this.wordSchema.find();
+    return allWords;
+  }
+
+  async getAdminsWords(email: string) {
+    const isAdmin = await this.userService.checkAdminRole(email);
+    if(isAdmin) return adminsWords
+    return 'user is not ADMIN'
+  }
 }
